74a46f9e1af899c77354c5cb66a2e965d6f94bc3
Derick Wells <wowells@up.edu.ph>
2021-11-30 12:35:16 +0800

    Add memory management

--- file_server.c
+++ file_server.c
@@ -4,25 +4,27 @@
 #include <string.h>
 #include <pthread.h>
 
-#define TYPE 0
+#define CMD 0
 #define PATH 1
-#define STR 2
-
+#define INPUT 2
+#define MAX_INP_SIZE 50
 
 struct worker_args {
-    char **cmd;
+    char cmd[MAX_INP_SIZE + 1];
+    char path[MAX_INP_SIZE + 1];
+    char input[MAX_INP_SIZE + 1];
 }worker_args;
 
 /**
     HELPER LIB
 **/
-void get_input(char *inp, char *cmd[]) {
+void get_input(char *inp, char **split) {
     scanf("%[^\n]%*c", inp);
 
     int i = 0;
     char *ptr = strtok(inp, " ");
     while (ptr != NULL) {
-        cmd[i++] = ptr;
+        strcpy(split[i++], ptr);
         ptr = strtok(NULL, " ");
     }
 }
@@ -48,37 +50,40 @@ void rand_sleep(int min, int max) {
     COMMANDS LIB
 **/
 void *worker_write(void *_args) {
+    struct worker_args *args = (struct worker_args *) _args;
+
     simulate_access();
 
     FILE *target_file;
-
-    struct worker_args *args = (struct worker_args *) _args;
-    (args->cmd)[PATH] = "/home/derick/acad/cs140/proj2/main/program.txt"; // temporary
-    target_file = fopen(args->cmd[PATH], "a");
+    strcpy(args->path, "/home/derick/acad/cs140/proj2/main/program.txt"); // temporary
+    target_file = fopen(args->path, "a");
 
     if (target_file == NULL) {
         printf("[ERR] worker_write fopen\n");   
         exit(1);
     }
 
-    fprintf(target_file,"%s", args->cmd[STR]);
+    fprintf(target_file,"%s", args->input);
     fclose(target_file);
+    free(args);
 }
 
 
 void *worker_read(void *_args) {
+    struct worker_args *args = (struct worker_args *) _args;
+
     simulate_access();
 
     FILE *from_file, *to_file;
-    struct worker_args *args = (struct worker_args *) _args;
-    args->cmd[PATH] = "/home/derick/acad/cs140/proj2/main/program.txt"; // temporary
-    from_file = fopen(args->cmd[PATH], "r");
+
+    strcpy(args->path, "/home/derick/acad/cs140/proj2/main/program.txt"); // temporary
+    from_file = fopen(args->path, "r");
     to_file = fopen("/home/derick/acad/cs140/proj2/main/read.txt", "a"); // temporary
 
     if (from_file == NULL) {
-        fprintf(to_file, "%s %s: FILE DNE\n", args->cmd[TYPE], args->cmd[PATH]);
+        fprintf(to_file, "%s %s: FILE DNE\n", args->cmd, args->path);
     } else {
-        fprintf(to_file, "%s %s: ", args->cmd[TYPE], args->cmd[PATH]);
+        fprintf(to_file, "%s %s: ", args->cmd, args->path);
         int copy;
         while ((copy = fgetc(from_file)) != EOF)
             fputc(copy, to_file);
@@ -90,29 +95,33 @@ void *worker_read(void *_args) {
     }
 
     fclose(to_file);
+
+    free(args);
 }
 
 
 void *worker_empty(void *_args) {
+    struct worker_args *args = (struct worker_args *) _args;
+
     simulate_access();
 
     FILE *from_file, *to_file;
-    struct worker_args *args = (struct worker_args *) _args;
-    args->cmd[PATH] = "/home/derick/acad/cs140/proj2/main/program.txt"; // temporary
-    from_file = fopen(args->cmd[PATH], "r");
+
+    strcpy(args->path, "/home/derick/acad/cs140/proj2/main/program.txt"); // temporary
+    from_file = fopen(args->path, "r");
     to_file = fopen("/home/derick/acad/cs140/proj2/main/empty.txt", "a"); // temporary
 
     if (from_file == NULL) {
-        fprintf(to_file, "%s %s: FILE DNE\n", args->cmd[TYPE], args->cmd[PATH]);
+        fprintf(to_file, "%s %s: FILE DNE\n", args->cmd, args->path);
         fclose(to_file);
         return 0;
     }
 
     int copy;
     if ((copy = fgetc(from_file)) == EOF) {
-        fprintf(to_file, "%s %s: FILE DNE\n", args->cmd[TYPE], args->cmd[PATH]);
+        fprintf(to_file, "%s %s: FILE DNE\n", args->cmd, args->path);
     } else {
-        fprintf(to_file, "%s %s: ", args->cmd[TYPE], args->cmd[PATH]);
+        fprintf(to_file, "%s %s: ", args->cmd, args->path);
         fputc(copy, to_file); // sleep per character???
         while ((copy = fgetc(from_file)) != EOF)
             fputc(copy, to_file);
@@ -123,12 +132,14 @@ void *worker_empty(void *_args) {
     fclose(from_file);
 
     // empty
-    from_file = fopen(args->cmd[PATH], "w");
+    from_file = fopen(args->path, "w");
     fclose(from_file);
 
     fclose(to_file);
 
     rand_sleep(7, 10);
+
+    free(args);
 }
 
 
@@ -136,27 +147,32 @@ void *worker_empty(void *_args) {
 /**
     MAIN
 **/
-int MAX_INP_SIZE = 50;
 int main() {
     char inp[] = "";
     while (1) {
-        char **cmd = malloc(3 * sizeof(char *));
-        for (int i = 0; i < 3; i++) {
-            cmd[i] = malloc((MAX_INP_SIZE + 1) * sizeof(char)); // free afterwards in thread
-        }
+        char **split = malloc(3 * sizeof(char *));
+        for (int i = 0; i < 3; i++)
+            split[i] = malloc((MAX_INP_SIZE + 1) * sizeof(char)); // free afterwards in thread
 
-        get_input(inp, cmd);
 
-        struct worker_args *args = malloc(sizeof(struct worker_args));
-        args->cmd = cmd;
+        get_input(inp, split);
 
-        pthread_t *tid = malloc(sizeof(tid));
-        if (strcmp(cmd[TYPE], "write") == 0) {
-            pthread_create(tid, NULL, worker_write, (void*)args);
-        } else if (strcmp(cmd[TYPE], "read") == 0) {
-            pthread_create(tid, NULL, worker_read, (void*)args);
-        } else if (strcmp(cmd[TYPE], "empty") == 0) {
-            pthread_create(tid, NULL, worker_empty, (void*)args);
+        struct worker_args *args = (struct worker_args *)malloc(sizeof(worker_args));
+        strcpy(args->cmd, split[CMD]);
+        strcpy(args->path, split[PATH]);
+        strcpy(args->input, split[INPUT]);
+
+        for (int i = 0; i < 3; i++)
+            free(split[i]);
+        free(split);
+
+        pthread_t tid;
+        if (strcmp(args->cmd, "write") == 0) {
+            pthread_create(&tid, NULL, worker_write, args);
+        } else if (strcmp(args->cmd, "read") == 0) {
+            pthread_create(&tid, NULL, worker_read, args);
+        } else if (strcmp(args->cmd, "empty") == 0) {
+            pthread_create(&tid, NULL, worker_empty, args);
         }
     }
 
